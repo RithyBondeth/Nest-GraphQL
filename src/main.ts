f87638cc79c2fbb246a27520e41708bad52f7b6c
Rithy Bondeth <rithybondeth999@gmail.com>
2025-10-30 14:39:10 +0700

    done validator in graphql

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EntityNotFoundFilter } from './entity-not-found/entity-not-found.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new EntityNotFoundFilter());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    
+  }))
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,15 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 @InputType()
 export class CreateUserInput {
+    @IsString()
+    @IsNotEmpty()
     @Field()
     username: string;
 
+    @IsEmail()
+    @IsNotEmpty()
     @Field()
     email: string;
 }
